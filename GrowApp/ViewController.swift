c47994ffcb70913ea439d0b04249e7b7514a9d11
Andrei <andrei@admins-MacBook-Pro.local>
2019-01-06 22:17:39 +0000

    Initial Commit

---
+++ GrowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrowApp
+//
+//  Created by Andrei on 06/01/2019.
+//  Copyright © 2019 Andrei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
